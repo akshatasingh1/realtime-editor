ac8b0a143f9ac04240c8ec75c2398b912041ab2d
Akshata Singh <singakshata1203@gmail.com>
2025-03-24 19:59:16 +0530

    worked on editor page

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,6 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Client from '../components/Client';
+import Editor from'../components/editor';
+
 const EditorPage=()=>{
-    return <div>Editor page</div>
+    const [clients,setClients]=useState([
+        {socketId:1,username:'Akshata S'},
+        {socketId:2,username:'John Doe'},
+        {socketId:3,username:'Jone Doe'},
+    ]);
+    return <div className="mainWrap">
+        <div className="aside">
+            <div className="asideInner">
+                <div className="logo">
+                    <img 
+                    className="logoImage"
+                    src="/code-sync.png" 
+                    alt="logo" />
+                </div>
+                <h3>Connected</h3>
+                <div className="clientsList">
+                    {
+                        clients.map((client)=>(
+                            <Client 
+                            key={client.socketId}
+                            username={client.username}/>
+                        ))
+                    }
+
+                </div>
+            </div>
+            <button className="btn copyBtn">Copy ROOM ID</button>
+            <button className="btn leaveBtn">Leave</button>
+        </div>
+        <div className="editorWrap">
+            <Editor />
+        </div>
+    </div>
     
 }
 export default EditorPage
