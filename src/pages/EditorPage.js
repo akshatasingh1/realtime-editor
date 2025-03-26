0a2f7a41dc7bee3b75c18e9e041bc0922489dc5a
Akshata Singh <singakshata1203@gmail.com>
2025-03-27 02:14:09 +0530

    added codemirror, socket.io and started with backend

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,8 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Client from '../components/Client';
-import Editor from'../components/editor';
+import Editor from '../components/Editor';
+import { initSocket } from '../socket';
 
 const EditorPage=()=>{
+    const socketRef=useRef(null);
+    useEffect(()=>{
+        const init=async()=>{
+            socketRef.current=await initSocket();
+            socketRef.current.emit('join');
+        }
+        init();
+    },[]);
     const [clients,setClients]=useState([
         {socketId:1,username:'Akshata S'},
         {socketId:2,username:'John Doe'},
